e4e865c24ed73abf7a0b609781e1265dff30828d
Cristian Tejada <ctejadah97@gmail.com>
2022-09-09 12:06:07 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from './components/APP'
+import './styles/global.scss'
+
+
+ReactDOM.render(<App />, document.getElementById('App'));
